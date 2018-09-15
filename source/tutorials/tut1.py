27066815eab031b1aa923f047a2851e3cce164b3
kpiaskowski <kar.piaskowski@gmail.com>
2018-09-15 21:57:19 +0200

    initial tutorials

---
+++ source/tutorials/tut1.py
@@ -0,0 +1,47 @@
+import random
+
+import pygame
+import numpy
+
+pygame.init()
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("My First Game")
+
+carryOn = True
+
+# The clock will be used to control how fast the screen updates
+clock = pygame.time.Clock()
+
+# -------- Main Program Loop -----------
+while carryOn:
+    # --- Main event loop
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            carryOn = False  # Flag that we are done so we exit this loop
+
+        # --- Game logic should go here
+
+        # --- Drawing code should go here
+        # First, clear the screen to white.
+    screen.fill(WHITE)
+    # The you can draw different shapes and lines or add text to your background stage.
+    pygame.draw.rect(screen, RED, [random.randint(0, 100), 200, 100, 70], 0)
+    pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
+    pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
+# Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
