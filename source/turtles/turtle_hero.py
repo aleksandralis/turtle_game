a75f1a21d1e082aa4f9f9a90244664c0dd03b4b9
aleksandralis <al.aleksandralis@gmail.com>
2018-09-27 22:21:10 +0200

    control pull request improvements

--- source/turtles/turtle_hero.py
+++ source/turtles/turtle_hero.py
@@ -1,42 +1,36 @@
 import pygame, random
 
-GREEN = (20, 255, 140)
-GREY = (210, 210, 210)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-PURPLE = (255, 0, 255)
-BLACK = (0, 0, 0)
-WIDTH = 100
-HEIGHT = 50
-ACC = 600
-TICK = 1/60.0
-SPEED_FAST = 300
-SPPED_SLOW = 150
-
 class Turtle(pygame.sprite.Sprite):
     # This class represents a turtle. It derives from the "Sprite" class in Pygame.
 
     def __init__(self, type, size_coeff, name, position):
         """
         :param type: turtle type (TODO: define)
-        :param size_coeff: SIZE_COEFF
+        :param size_coeff: SIZE_COEFF - turtle size scaling
         :param name: name strinf
-        :param position: creation postition in pixels - tuple
+        :param position: creation postition in pixels - tuple (x,y), starting from left, top corner
         """
         # Call the parent class (Sprite) constructor
         super().__init__()
-
+        self.GREEN = (20, 255, 140)
+        self.GREY = (210, 210, 210)
+        self.WHITE = (255, 255, 255)
+        self.RED = (255, 0, 0)
+        self.PURPLE = (255, 0, 255)
+        self.BLACK = (0, 0, 0)
+        self.WIDTH = 100
+        self.HEIGHT = 50
         # Pass in the name of the turtle, type, size, and its x and y position
         # Set the background color and set it to be transparent
         self.name = name
-        self.image = pygame.Surface([WIDTH * size_coeff, HEIGHT * size_coeff])
-        self.image.fill(WHITE)
-        self.image.set_colorkey(WHITE)
+        self.image = pygame.Surface([self.WIDTH * size_coeff, self.HEIGHT * size_coeff])
+        self.image.fill(self.WHITE)
+        self.image.set_colorkey(self.WHITE)
 
         self.size = size_coeff
         self.type = type
         # Draw the turtle (a rectangle!)
-        pygame.draw.rect(self.image, BLACK, [0, 0, WIDTH * size_coeff, HEIGHT * size_coeff])
+        pygame.draw.rect(self.image, self.BLACK, [0, 0, self.WIDTH * size_coeff, self.HEIGHT * size_coeff])
         # Instead we could load a proper picture of a turtle...
         # self.image = pygame.image.load("turtle.png").convert_alpha()
 
@@ -62,7 +56,11 @@ class Turtle(pygame.sprite.Sprite):
 class TurtleHero(Turtle):
     def __init__(self, type, size_coeff, name, position):
         super().__init__(type, size_coeff, name, position)
-        self.is_jumping = 0
+        self.ACC = 600
+        self.TICK = 1 / 60.0
+        self.SPEED_FAST = 300
+        self.SPPED_SLOW = 150
+        self.is_jumping = 0 #jumping state machine: 0 - no jumping, 1 - jump up, 2 - jump down
         self.dist_to_jump = 0
         self.initial_y = 0
         self.jump_counter = 0
@@ -70,7 +68,6 @@ class TurtleHero(Turtle):
         self.speed = 0
         self.speed_act = 0
         self.speed_target = 0
-        self.tmp_x_float = 0
 
     def init_jump(self, initial_v, grav_acc):
         self.is_jumping = 1
@@ -84,39 +81,39 @@ class TurtleHero(Turtle):
         self.speed = speed
 
     def init_move_right(self):
-        self.speed_target = SPPED_SLOW
+        self.speed_target = self.SPPED_SLOW
 
     def init_move_fast_right(self):
-        self.speed_target = SPEED_FAST
+        self.speed_target = self.SPEED_FAST
 
     def init_move_left(self):
-        self.speed_target = -1 * SPPED_SLOW
+        self.speed_target = -1 * self.SPPED_SLOW
 
     def init_move_fast_left(self):
-        self.speed_target = -1*SPEED_FAST
+        self.speed_target = -1 * self.SPEED_FAST
 
     def stop_move(self):
         self.speed_target = 0
 
     def move(self):
-        if self.speed_target == round(self.speed_act,1):
-            self.tmp_x_float = self.rect.x + self.speed_target * (TICK)
+        if self.speed_target == round(self.speed_act,1): #constant motion
+            self.tmp_x_float = self.rect.x + self.speed_target * (self.TICK)
             self.rect.x = self.tmp_x_float + 0.5
-        elif self.speed_target > self.speed_act:
-            self.tmp_x_float = self.rect.x + self.speed_act * (TICK) + ACC *(TICK)**2/2
-            self.speed_act = self.speed_act + ACC *(TICK)
+        elif self.speed_target > self.speed_act: #uniformly accelerated motion
+            self.tmp_x_float = self.rect.x + self.speed_act * (self.TICK) + self.ACC *(self.TICK)**2/2
+            self.speed_act = self.speed_act + self.ACC *(self.TICK)
             self.rect.x = self.tmp_x_float + 0.5
-        elif self.speed_target < self.speed_act:
-            self.tmp_x_float = self.rect.x + self.speed_act * (TICK) - ACC *(TICK)**2/2
-            self.speed_act = self.speed_act - ACC*(TICK)
+        elif self.speed_target < self.speed_act: #uniformly deleted motion
+            self.tmp_x_float = self.rect.x + self.speed_act * (self.TICK) - self.ACC *(self.TICK)**2/2
+            self.speed_act = self.speed_act - self.ACC*(self.TICK)
             self.rect.x = self.tmp_x_float + 0.5
-        if self.speed_act < 0.01 and self.speed_act > -0.01:
+        if self.speed_act < 0.01 and self.speed_act > -0.01: #if zero (precision 0.1) - stop entirely
             self.speed_act = 0.0
         print("actual " + str(self.speed_act), "target " + str(self.speed_target))
 
     def jump(self, initial_v, grav_acc):
         if self.is_jumping == 1:
-            self.rect.y = self.initial_y - initial_v * (self.jump_counter * TICK) + grav_acc * (self.jump_counter * TICK) ** 2 / 2
+            self.rect.y = self.initial_y - initial_v * (self.jump_counter * self.TICK) + grav_acc * (self.jump_counter * self.TICK) ** 2 / 2
             print(self.rect.y)
             self.jump_counter = self.jump_counter + 1
             if self.initial_y - self.rect.y >= self.dist_to_jump:
@@ -125,7 +122,7 @@ class TurtleHero(Turtle):
                 print("down")
                 self.rect.y = self.initial_y - self.dist_to_jump
         elif self.initial_y >= self.rect.y and self.is_jumping == 2:
-            self.rect.y = self.initial_y - self.dist_to_jump + grav_acc * (self.jump_counter * TICK) ** 2 / 2
+            self.rect.y = self.initial_y - self.dist_to_jump + grav_acc * (self.jump_counter * self.TICK) ** 2 / 2
             print(self.rect.y)
             self.jump_counter = self.jump_counter + 1
             if self.initial_y <= self.rect.y:
