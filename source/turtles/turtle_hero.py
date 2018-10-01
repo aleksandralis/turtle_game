41085cf358f5427c9123739cd0ebffdad5b27967
aleksandralis <al.aleksandralis@gmail.com>
2018-10-01 22:11:31 +0200

    turtle amim code refactor

--- source/turtles/turtle_hero.py
+++ source/turtles/turtle_hero.py
@@ -26,12 +26,12 @@ class Turtle(pygame.sprite.Sprite):
         self.BLACK = (0, 0, 0)
         self.WIDTH = 64
         self.HEIGHT = 64
-        self.init_sub = (0,0)
+        self.init_sub = (0,0) #default icon column and row
 
         # Pass in the name of the turtle, type, size, and its x and y position
         # Set the background color and st it to be transparent
         self.name = name
-        self.image = self.get_image(self.WIDTH * self.init_sub[0] , self.HEIGHT * self.init_sub[1], self.WIDTH, self.HEIGHT)#pygame.Surface([self.WIDTH * size_coeff, self.HEIGHT * size_coeff])
+        self.image = self.get_image(self.WIDTH * self.init_sub[0] , self.HEIGHT * self.init_sub[1], self.WIDTH, self.HEIGHT)
         # self.image.fill(self.WHITE)
         # self.image.set_colorkey(self.WHITE)
 
@@ -72,7 +72,7 @@ class TurtleHero(Turtle):
         self.TICK = 1 / 60.0
         self.SPEED_FAST = 300
         self.SPPED_SLOW = 150
-        self.is_jumping = JumpStates.IDLE #jumping state machine: 0 - no jumping, 1 - jump up, 2 - jump down
+        self.is_jumping = JumpStates.IDLE# jumping state machine: 0 - no jumping, 1 - jump up, 2 - jump down
         self.dist_to_jump = 0
         self.initial_y = 0
         self.jump_counter = 0
@@ -81,13 +81,13 @@ class TurtleHero(Turtle):
         self.speed_act = 0
         self.speed_target = 0
         ######animation##########
-        self.init_sub = (7,0)
-        self.image = self.get_image(self.WIDTH * self.init_sub[0] , self.HEIGHT * self.init_sub[1], self.WIDTH, self.HEIGHT)#pygame.Surface([self.WIDTH * size_coeff, self.HEIGHT * size_coeff])
-        self.walk_r = [(6,0), (7,0), (8,0)]
-        self.i_count = 0
-        self.hide_anim = (0,3)
-        self.die_anim = (6,5)
-        self.right = 1
+        self.init_sub = (7,0)# default icon column and row
+        self.image = self.get_image(self.WIDTH * self.init_sub[0], self.HEIGHT * self.init_sub[1], self.WIDTH, self.HEIGHT)
+        self.walk_r = [(6, 0), (7, 0), (8, 0)]  # icons of walk right column and row
+        self.i_count = 0  # counter for walking speed
+        self.hide_anim = (0, 3)  # icon of hide column and row
+        self.die_anim = (6, 5)  # icon of die column and row
+        self.right = 1  # turtle waling right flag
 
     def get_image(self, x, y, w, h):
         return self.image_sheet.subsurface((x, y, w, h))
@@ -123,22 +123,21 @@ class TurtleHero(Turtle):
         self.speed_target = 0
 
     def move(self):
-        if self.speed_target == round(self.speed_act,1): #constant motion
+        if self.speed_target == round(self.speed_act, 1):  # constant motion
             self.tmp_x_float = self.rect.x + self.speed_target * (self.TICK)
             self.rect.x = self.tmp_x_float + 0.5
-        elif self.speed_target > self.speed_act: #uniformly accelerated motion
-            self.tmp_x_float = self.rect.x + self.speed_act * (self.TICK) + self.ACC *(self.TICK)**2/2
-            self.speed_act = self.speed_act + self.ACC *(self.TICK)
+        elif self.speed_target > self.speed_act:  # uniformly accelerated motion
+            self.tmp_x_float = self.rect.x + self.speed_act * (self.TICK) + self.ACC * (self.TICK) ** 2 / 2
+            self.speed_act = self.speed_act + self.ACC * (self.TICK)
             self.rect.x = self.tmp_x_float + 0.5
-        elif self.speed_target < self.speed_act: #uniformly deccelerated motion
-            self.tmp_x_float = self.rect.x + self.speed_act * (self.TICK) - self.ACC *(self.TICK)**2/2
-            self.speed_act = self.speed_act - self.ACC*(self.TICK)
+        elif self.speed_target < self.speed_act:  # uniformly deccelerated motion
+            self.tmp_x_float = self.rect.x + self.speed_act * (self.TICK) - self.ACC * (self.TICK) ** 2 / 2
+            self.speed_act = self.speed_act - self.ACC * (self.TICK)
             self.rect.x = self.tmp_x_float + 0.5
-        if self.speed_act < 0.01 and self.speed_act > -0.01: #if zero (precision 0.1) - stop entirely
+        if self.speed_act < 0.01 and self.speed_act > -0.01:  # if zero (precision 0.1) - stop entirely
             self.speed_act = 0.0
         self.update_moving_animation()
-        #print("actual " + str(self.speed_act), "target " + str(self.speed_target))
-
+        # print("actual " + str(self.speed_act), "target " + str(self.speed_target))
 
     def jump(self, initial_v, grav_acc):
         if self.is_jumping == JumpStates.UP:
@@ -163,28 +162,28 @@ class TurtleHero(Turtle):
                 print("end")
 
     def update_anim_stop_rigth(self):
-        self.image = self.get_image(self.walk_r[1][0]*self.WIDTH, self.walk_r[1][1]*self.HEIGHT,self.WIDTH,self.HEIGHT)
+        self.image = self.get_image(self.walk_r[1][0] * self.WIDTH, self.walk_r[1][1] * self.HEIGHT, self.WIDTH, self.HEIGHT)
 
     def update_anim_stop_left(self):
-        self.image = self.get_image(self.walk_r[1][0]*self.WIDTH, self.walk_r[1][1]*self.HEIGHT,self.WIDTH,self.HEIGHT)
+        self.image = self.get_image(self.walk_r[1][0] * self.WIDTH, self.walk_r[1][1] * self.HEIGHT, self.WIDTH, self.HEIGHT)
         self.image = pygame.transform.flip(self.image, True, False)
 
     def update_anim_walk_right(self, iter):
-        count = self.i_count//iter
-        self.image = self.get_image(self.walk_r[count][0]*self.WIDTH, self.walk_r[count][1]*self.HEIGHT,self.WIDTH,self.HEIGHT)
-        self.i_count = (self.i_count + 1)%len(self.walk_r*iter)
+        count = self.i_count // iter
+        self.image = self.get_image(self.walk_r[count][0] * self.WIDTH, self.walk_r[count][1] * self.HEIGHT, self.WIDTH, self.HEIGHT)
+        self.i_count = (self.i_count + 1) % len(self.walk_r * iter)
 
     def update_anim_walk_left(self, iter):
-        count = self.i_count//iter
-        self.image = self.get_image(self.walk_r[count][0]*self.WIDTH, self.walk_r[count][1]*self.HEIGHT,self.WIDTH,self.HEIGHT)
+        count = self.i_count // iter
+        self.image = self.get_image(self.walk_r[count][0] * self.WIDTH, self.walk_r[count][1] * self.HEIGHT, self.WIDTH, self.HEIGHT)
         self.image = pygame.transform.flip(self.image, True, False)
-        self.i_count = (self.i_count + 1)%len(self.walk_r*iter)
+        self.i_count = (self.i_count + 1) % len(self.walk_r * iter)
 
     def update_hide_anim(self):
-        self.image = self.get_image(self.hide_anim[0]*self.WIDTH, self.hide_anim[1]*self.HEIGHT,self.WIDTH,self.HEIGHT)
+        self.image = self.get_image(self.hide_anim[0] * self.WIDTH, self.hide_anim[1] * self.HEIGHT, self.WIDTH, self.HEIGHT)
 
     def update_die_anim(self):
-        self.image = self.get_image(self.die_anim[0]*self.WIDTH, self.die_anim[1]*self.HEIGHT,self.WIDTH,self.HEIGHT)
+        self.image = self.get_image(self.die_anim[0] * self.WIDTH, self.die_anim[1] * self.HEIGHT, self.WIDTH, self.HEIGHT)
 
     def update_moving_animation(self):
         if self.speed_act > 0.01:
